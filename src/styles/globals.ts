eb416658c7704af859d5a9fc192bdb512ccc3f21
natanarau <natan.mca@gmail.com>
2022-07-06 12:27:43 -0300

    Base creation of the Reviva project

---
+++ src/styles/globals.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle `
+  html {
+    font-family: 'Inter', sans-serif;
+    font-size: 62.5%;
+  }
+  h1 {
+    font-size: 3.5rem;
+  }
+  h2 {
+    font-size: 2rem;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    color: #261E1E;
+    text-decoration: none;
+  }
+`

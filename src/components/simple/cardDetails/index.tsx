bed72a7cc6b923beb5f57e25d0a51203cb13adbf
ITALLA FELYNE MENDES FERREIRA <4057002@riachuelo.com.br>
2022-07-10 11:45:39 -0300

    Inserindo cartão da página detalhes

---
+++ src/components/simple/cardDetails/index.tsx
@@ -0,0 +1,13 @@
+import * as S from '../../complex/card/styles';
+import * as A from '../contentCard/styles';
+
+
+export default function CardDetails() {
+    return (
+        <S.card>
+            <A.containerFlag>
+                <A.flag src='/image/LogoCartao.png' />
+            </A.containerFlag>
+        </S.card>
+    );
+} 
---
+++ src/components/simple/cardDetails/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const card = styled.section`
+    background: #3A8BEB;
+    border-radius: 1.1rem;
+    
+    height: 12rem;
+    left: 2.313rem;
+    margin-left: auto;
+    margin-right: auto; 
+    width: 19rem;    
+`;

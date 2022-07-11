1e758e590165948e14a438f52a7c9b31486a11a8
Osmarv <osmarv@gmail.com>
2022-07-11 10:13:12 -0300

    FIX: January initial loading

--- src/components/complex/MenuMoth/index.tsx
+++ src/components/complex/MenuMoth/index.tsx
@@ -33,6 +33,7 @@ export default function MenuMonth() {
   };
 
   const { currentMonth, setCurrentMonth } = useDataUsers()
+  setCurrentMonth(1);
 
   const handleMonth = (numberMonth: number) => {
     console.log(numberMonth)

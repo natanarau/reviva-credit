71de41b935c8f3e9e30a4e2dec2b76eae34a42e2
ITALLA FELYNE MENDES FERREIRA <4057002@riachuelo.com.br>
2022-07-11 12:16:10 -0300

    Atualizando cartão

--- src/components/simple/cardDetails/index.tsx
+++ src/components/simple/cardDetails/index.tsx
@@ -1,10 +1,15 @@
 import * as S from '../../complex/card/styles';
 import * as A from '../contentCard/styles';
 
+interface cardProps {
+    width: string;
+    height: string;
+    borderRadius: string;
+}
 
-export default function CardDetails() {
+export default function CardDetails(props: cardProps) {
     return (
-        <S.card>
+        <S.card width ={"6.3rem"} height ={"3.7rem"} borderRadius ={"0.5rem"} >
             <A.containerFlag>
                 <A.flag src='/image/LogoCartao.png' />
             </A.containerFlag>

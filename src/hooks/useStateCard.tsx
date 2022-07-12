cffaf8511bd12846d0ef25bb751a5d63f18ec62f
natanarau <natan.mca@gmail.com>
2022-07-12 13:17:43 -0300

    Feature: Month color change

---
+++ src/hooks/useStateCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const useStateCard = () => {
+  const [stateCard, setStateCard] = React.useState<string>()
+  const [color, serColor] = React.useState<string>()
+  
+  const date = new Date();
+  const day = Number(date.getDate());
+
+  const dayCard = (dayCard: number) => {
+    const cardTurnedOver = dayCard && dayCard - 5
+    if(cardTurnedOver && day >= cardTurnedOver && day < dayCard) {
+      setStateCard('Fechado')
+      serColor('#E83151')
+    } else if(cardTurnedOver && day > dayCard){
+      setStateCard('Pago')
+      serColor('#CDCDCD')
+    } else if(cardTurnedOver && day < cardTurnedOver){
+      setStateCard('Aberto')
+      serColor('#06D6A0')
+    }
+  }
+ 
+  return {stateCard, dayCard, color}
+}

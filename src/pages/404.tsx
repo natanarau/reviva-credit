40fbd315a3f1c3f0bac21541d391945c63b12e21
natanarau <natan.mca@gmail.com>
2022-07-12 15:55:49 -0300

    Update template 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,13 @@
 import NotFound from 'components/simple/404'
 import React from 'react'
+import Template from 'templates/index'
 
-type Props = {}
-
-export default function Error404({}: Props) {
+export default function Error404() {
   return (
     <>
+    <Template>
       <NotFound />
+    </Template>
     </>
   )
 }

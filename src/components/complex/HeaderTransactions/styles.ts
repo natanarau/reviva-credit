12e62bd174a795f783ce824e69af25d02c15af3c
Osmarv <osmarv@gmail.com>
2022-07-13 16:54:09 -0300

    FIX: Transactions header

---
+++ src/components/complex/HeaderTransactions/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  width: 375px;
+`
+export const HelloText = styled.div`
+  margin: 2rem 0 2rem 2rem;
+`
+export const H2 = styled.h2`
+  font-size: 1rem;
+  font-weight: 400;
+`
+export const HeaderImg = styled.img`
+  height: 5rem;
+  width: 5rem;
+`
+export const Strong = styled.strong `
+  font-weight: 700;
+`
